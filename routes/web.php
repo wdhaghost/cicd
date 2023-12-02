3a26e5876db7fa0625ccde063c7acc7d19211822
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2023-12-02 17:00:54 +0100

    Add route for StudentController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\StudentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+
+Route::resource('students',StudentController::class);
