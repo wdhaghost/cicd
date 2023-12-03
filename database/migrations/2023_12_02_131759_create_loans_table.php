71d3b05bba688a5875229632f4bf5c05e099a2a9
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2023-12-03 16:37:51 +0100

    Add migration for creating loans table

---
+++ database/migrations/2023_12_02_131759_create_loans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->date('loan_date');
+            $table->date('return_date');
+            $table->boolean('returned')->default(false);
+            $table->unsignedBigInteger('equipment_id');
+            $table->unsignedBigInteger('student_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};
