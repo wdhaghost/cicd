e3caf0c19402d57baf471264ea511460b1c64025
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2023-12-05 09:12:20 +0100

    Add return email template

---
+++ resources/views/emails/return.blade.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Retour de prêt </title>
+</head>
+<body>
+    <h1>Retour</h1>
+    <p>
+        Parfait, vous avez rendu : {{ $equipmentName }} le {{date('d-m-Y')}}.
+    </p>
+   
+</body>
+</html>
